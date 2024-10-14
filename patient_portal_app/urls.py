1a508d53b1c4c4be269848f79ba2780d35cdeb8d
Parimal Ingle <parimal.ingle18@gmail.com>
2024-10-14 00:27:07 -0700

    finalized login and register page layout

--- patient_portal_app/urls.py
+++ patient_portal_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.home, name='home'), # to reference or create link to this page on another page, this 'name' reference is required.
-    path('login/', views.login, name='login'),
+    path('', views.login, name='login'), # to reference or create link to this page on another page, this 'name' reference is required.
+    # path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
 ]
--- patient_portal_app/views.py
+++ patient_portal_app/views.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render
-
-def home(req):
-    welcome_text = "Welcome to Patient Portal."
-    return render(req, 'home.html', {'welcome_text': welcome_text}) # {} for passing python code/args to our page.
+from django.shortcuts import render    
 
 def login(req):
-    return render(req, 'login.html', {})
+    welcome_text = "Welcome to Patient Portal."
+    return render(req, 'login.html', {'welcome_text': welcome_text}) # {} for passing python code/args to our page.
 
 def register(req):
     return render(req, 'register.html', {})
