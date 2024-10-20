139742acc6a569da5865a0be5cc975c86a9114f5
Parimal Ingle <parimal.ingle18@gmail.com>
2024-10-19 20:10:37 -0700

    user registration and authentication backend completed

--- patient_portal_app/urls.py
+++ patient_portal_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.login, name='login'), # to reference or create link to this page on another page, this 'name' reference is required.
     # path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
+    path('landing/', views.landing, name='landing'),
+    path('logout/', views.logout, name='logout'),
 ]
--- patient_portal_app/views.py
+++ patient_portal_app/views.py
@@ -1,10 +1,55 @@
-from django.shortcuts import render  
+from django.shortcuts import redirect, render  
 from .models import User  
 from .forms import registerForm
+from django.contrib import messages
+
+user_loggedIn = False
 
 def login(req):
-    welcome_text = "Welcome to Patient Portal."
-    return render(req, 'login.html', {'welcome_text': welcome_text}) # {} for passing python code/args to our page.
+    global user_loggedIn
+    if req.method == 'POST':
+        print(req.POST['user_email'])
+        print(req.POST['user_pw'])
+        if User.objects.filter(user_email=req.POST['user_email'], user_pw=req.POST['user_pw']):
+            user_loggedIn = True
+            print("login success!!")
+            return redirect('landing')
+        else:
+            messages.success(req, ('User or Password does not match. Please try again!'))
+    else:
+        if not user_loggedIn:
+            return render(req, 'login.html', {})
+        else:
+            return render(req, 'landing.html', {})
 
 def register(req):
-    return render(req, 'register.html', {})
+    if req.method == 'POST':
+        new_user = registerForm(req.POST or None)
+        if new_user.is_valid():
+            if User.objects.filter(user_email=new_user['user_email'].value()):
+                messages.success(req, ('User with this E-mail id already exists! Please sign in to continue.'))
+                return redirect('login')
+            elif req.POST['user_pw'] != req.POST['user_cnf_pw']:
+                print('pw match error!')
+                messages.success(req, ('Passwords do not match! Please try again.'))
+                return redirect('register')
+            else:
+                new_user.save()
+                print("saved success!!!")
+                messages.success(req, ('User Saved successfully! Please continue to sign in.'))
+                return redirect('login')
+        else:
+            print("error creating user!")
+    else:
+        return render(req, 'register.html', {})
+    
+def landing(req):
+    return render(req, 'landing.html', {})
+
+def logout(req):
+    global user_loggedIn
+    user_loggedIn = False
+    if 'register' in req.META['HTTP_REFERER']:
+        return register(req)
+    else:
+        return login(req)
